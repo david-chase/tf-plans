f923218b9eeef4c0cd5c4221c139956b21a1e173
David Chase <dchase@hotmail.com>
2024-10-21 12:24:59 -0400

    2024.10.21;12:24;Automated sync

--- k8s-EKS/main.tf
+++ k8s-EKS/main.tf
@@ -186,6 +186,9 @@ module "eks" {
       max_size     = 3
       desired_size = 1
 
+ #     labels = {
+ #       "eks.amazonaws.com/nodegroup" = "node-group-1"
+ #     }
     }
 
     two = {
@@ -197,7 +200,10 @@ module "eks" {
       min_size     = 1
       max_size     = 2
       desired_size = 1
-   
+
+#      labels = {
+#        "eks.amazonaws.com/nodegroup" = "node-group-2"
+#      }      
     }
     
     tags =  {
